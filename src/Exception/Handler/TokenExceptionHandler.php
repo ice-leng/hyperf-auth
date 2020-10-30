b0457211e399833b5593481abbc6deea6781532b
冷斌 <xykxyk2008@163.com>
2020-10-31 01:10:13 +0800

    add exception handler

---
+++ src/Exception/Handler/TokenExceptionHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Lengbin\Hyperf\Exception\Handler;
+
+use Hyperf\ExceptionHandler\ExceptionHandler;
+use Lengbin\Auth\Exception\InvalidTokenException;
+use Lengbin\Hyperf\Common\Error\CommentErrorCode;
+use Lengbin\Hyperf\Common\Exception\Handler\ExceptionHandlerTrait;
+use Lengbin\Jwt\Exception\ExpiredJwtException;
+use Lengbin\Jwt\Exception\InvalidJwtException;
+use Psr\Http\Message\ResponseInterface;
+use Throwable;
+
+class TokenExceptionHandler extends ExceptionHandler
+{
+    use ExceptionHandlerTrait;
+
+    public function handle(Throwable $throwable, ResponseInterface $response)
+    {
+        $this->stopPropagation();
+        $this->formatLog($throwable);
+        $error = $throwable instanceof ExpiredJwtException ? CommentErrorCode::TOKEN_EXPIRED() : CommentErrorCode::INVALID_TOKEN();
+        return $this->response->fail($error->getValue(), $error->getMessage());
+    }
+
+    public function isValid(Throwable $throwable): bool
+    {
+        return $throwable instanceof InvalidTokenException || $throwable instanceof InvalidJwtException || $throwable instanceof ExpiredJwtException;
+    }
+}
